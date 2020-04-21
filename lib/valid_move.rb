5080fcf1c97a5fe84f4c635952719535d62bc7dd
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-21 14:53:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,6 @@ def valid_move?(board, index)
   !position_taken?(board, index)
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
