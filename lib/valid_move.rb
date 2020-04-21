78e483015a30a0e3053d060d6e1bc1d62d6cb5f8
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-21 14:53:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,12 @@
 # code your #valid_move? method here
+
 def valid_move?(board, index)
-  index > board.length - 1 ? false : !position_taken?(board, index)
+  # If move is not on board
+  if index > board.length - 1 ? false : 
+    return false
+  end
+
+  !position_taken?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
